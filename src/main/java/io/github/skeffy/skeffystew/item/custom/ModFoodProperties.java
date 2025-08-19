94b48806c2ac21acc759bff13f7322c50791def1
Noah Stevens <stevens_nas@yahoo.com>
2025-08-18 22:28:20 -0400

    Initial commit

---
+++ src/main/java/io/github/skeffy/skeffystew/item/custom/ModFoodProperties.java
@@ -0,0 +1,13 @@
+package io.github.skeffy.skeffystew.item.custom;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoodProperties {
+    public static final FoodProperties HEALING_STEW = new FoodProperties.Builder().nutrition(4)
+            .saturationMod(1f).effect(() -> new MobEffectInstance(MobEffects.HEAL, 1), 100).build();
+
+    public static final FoodProperties BLINDING_STEW = new FoodProperties.Builder().nutrition(4)
+            .saturationMod(1f).effect(() -> new MobEffectInstance(MobEffects.BLINDNESS, 24000), 100).build();
+}
