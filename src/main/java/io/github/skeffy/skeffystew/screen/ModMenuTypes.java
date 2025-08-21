7d4b071525d6cd4ce47f3916e4be107b5ce70790
Noah Stevens <stevens_nas@yahoo.com>
2025-08-21 00:15:08 -0400

    Initial commit

---
+++ src/main/java/io/github/skeffy/skeffystew/screen/ModMenuTypes.java
@@ -0,0 +1,27 @@
+package io.github.skeffy.skeffystew.screen;
+
+import io.github.skeffy.skeffystew.SkeffyStews;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.network.IContainerFactory;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModMenuTypes {
+    public static final DeferredRegister<MenuType<?>> MENUS =
+            DeferredRegister.create(ForgeRegistries.MENU_TYPES, SkeffyStews.MOD_ID);
+
+    public static final RegistryObject<MenuType<StewPotMenu>> STEW_POT_MENU =
+            registerMenuType("stew_pot_menu", StewPotMenu::new);
+
+    private static <T extends AbstractContainerMenu>RegistryObject<MenuType<T>> registerMenuType(String name, IContainerFactory<T> factory) {
+        return MENUS.register(name, () -> IForgeMenuType.create(factory));
+    }
+
+    public static void register(IEventBus eventBus) {
+        MENUS.register(eventBus);
+    }
+}
