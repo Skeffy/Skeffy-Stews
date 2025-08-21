5d2130d2d7d460290e9f175542307f2ae7a1de60
Noah Stevens <stevens_nas@yahoo.com>
2025-08-21 00:16:04 -0400

    Initial commit

---
+++ src/main/java/io/github/skeffy/skeffystew/block/entity/StewPotBlockEntity.java
@@ -0,0 +1,173 @@
+package io.github.skeffy.skeffystew.block.entity;
+
+import io.github.skeffy.skeffystew.item.ModItems;
+import io.github.skeffy.skeffystew.screen.StewPotMenu;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class StewPotBlockEntity extends BlockEntity implements MenuProvider {
+    private final ItemStackHandler itemHandler = new ItemStackHandler(5);
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+    private int OUTPUT_SLOT = 5;
+    private int BOWL_SLOT = 1;
+    private int INGREDIENT_SLOT_1 = 2;
+    private int INGREDIENT_SLOT_2 = 3;
+    private int FUEL_SLOT = 4;
+    protected final ContainerData data;
+    private int progress = 0;
+    private int maxProgress = 78;
+
+
+    public StewPotBlockEntity(BlockPos pPos, BlockState pBlockState) {
+        super(ModBlockEntities.STEW_POT_BLOCK_ENTITY.get(), pPos, pBlockState);
+        this.data = new ContainerData() {
+            @Override
+            public int get(int pIndex) {
+                return switch (pIndex) {
+                    case 0 -> StewPotBlockEntity.this.progress;
+                    case 1 -> StewPotBlockEntity.this.maxProgress;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int pIndex, int pValue) {
+                switch (pIndex) {
+                    case 0 -> StewPotBlockEntity.this.progress = pValue;
+                    case 1 -> StewPotBlockEntity.this.maxProgress = pValue;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if(cap == ForgeCapabilities.ITEM_HANDLER) {
+            return lazyItemHandler.cast();
+        }
+
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        lazyItemHandler.invalidate();
+    }
+
+    public void drops() {
+        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
+        for(int i =0; i < itemHandler.getSlots(); i++) {
+            inventory.setItem(i, itemHandler.getStackInSlot(i));
+        }
+
+        Containers.dropContents(this.level, this.worldPosition, inventory);
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("block.skeffystews.stew_pot");
+    }
+
+    @Override
+    public @Nullable AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
+        return new StewPotMenu(pContainerId, pPlayerInventory, this, this.data);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        pTag.put("inventory", itemHandler.serializeNBT());
+        pTag.putInt("stew_pot.progress", progress);
+        super.saveAdditional(pTag);
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+        itemHandler.deserializeNBT(pTag.getCompound("inventory"));
+        progress = pTag.getInt("stew_pot.progress");
+    }
+
+
+    public void tick(Level pLevel, BlockPos pPos, BlockState pState) {
+        if(hasRecipe()) {
+            increaseCraftingProgress();
+            setChanged(pLevel, pPos, pState);
+
+            if(hasProgressFinished()) {
+                craftItem();
+                resetProgress();
+            }
+        } else {
+            resetProgress();
+        }
+    }
+
+    private void resetProgress() {
+        progress = 0;
+    }
+
+    private void craftItem() {
+        ItemStack result = new ItemStack(ModItems.HEALING_STEW.get());
+        this.itemHandler.extractItem(BOWL_SLOT, 1, false);
+        this.itemHandler.extractItem(INGREDIENT_SLOT_1, 1, false);
+        this.itemHandler.extractItem(INGREDIENT_SLOT_2, 1, false);
+
+        this.itemHandler.setStackInSlot(OUTPUT_SLOT, new ItemStack(result.getItem(),
+                this.itemHandler.getStackInSlot(OUTPUT_SLOT).getCount() + result.getCount()));
+    }
+
+    private boolean hasProgressFinished() {
+        return progress >= maxProgress;
+    }
+
+    private void increaseCraftingProgress() {
+        progress++;
+    }
+
+    private boolean hasRecipe() {
+        ItemStack result = new ItemStack(ModItems.HEALING_STEW.get());
+        boolean hasCraftingItems = false;
+        return hasCraftingItems && canInsertAmountIntoOutput(result.getCount()) && canInsertItemIntoOutput(result.getItem());
+    }
+
+    private boolean canInsertItemIntoOutput(Item item) {
+        return this.itemHandler.getStackInSlot(OUTPUT_SLOT).isEmpty() || this.itemHandler.getStackInSlot(OUTPUT_SLOT).is(item);
+    }
+
+    private boolean canInsertAmountIntoOutput(int count) {
+        return this.itemHandler.getStackInSlot(OUTPUT_SLOT).getCount() + count <= this.itemHandler.getStackInSlot(OUTPUT_SLOT).getMaxStackSize();
+    }
+}
