c15f6a4a7052c728de4e4b52e7ec4e4206335108
Noah Stevens <stevens_nas@yahoo.com>
2025-09-10 21:32:30 -0400

    Create custom tag for bowl items

---
+++ src/main/java/io/github/skeffy/skeffystew/util/ModTags.java
@@ -0,0 +1,18 @@
+package io.github.skeffy.skeffystew.util;
+
+import io.github.skeffy.skeffystew.SkeffyStews;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class ModTags {
+    public static class Items {
+        public static final TagKey<Item> BOWL_ITEMS = tag("bowl_items");
+
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(SkeffyStews.MOD_ID, name));
+        }
+    }
+}
