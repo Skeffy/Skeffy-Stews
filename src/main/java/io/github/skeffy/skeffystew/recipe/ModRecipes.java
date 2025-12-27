e2c43e331d710764f59e08d0e351705660c00d1c
Noah Stevens <stevens_nas@yahoo.com>
2025-12-27 12:26:43 -0500

    Remove register and registry object for type

--- src/main/java/io/github/skeffy/skeffystew/recipe/ModRecipes.java
+++ src/main/java/io/github/skeffy/skeffystew/recipe/ModRecipes.java
@@ -2,7 +2,6 @@ package io.github.skeffy.skeffystew.recipe;
 
 import io.github.skeffy.skeffystew.SkeffyStews;
 import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,21 +10,11 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModRecipes {
     public static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS =
             DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, SkeffyStews.MOD_ID);
-    public static final DeferredRegister<RecipeType<?>> TYPES =
-            DeferredRegister.create(ForgeRegistries.RECIPE_TYPES, SkeffyStews.MOD_ID);
 
     public static final RegistryObject<RecipeSerializer<StewCookingRecipe>> STEW_COOKING_SERIALIZER =
-            SERIALIZERS.register("stew_cooking", () -> new StewCookingRecipe.Serializer(200));
-    public static final RegistryObject<RecipeType<StewCookingRecipe>> STEW_COOKING_TYPE =
-            TYPES.register("stew_cooking", () -> new RecipeType<>() {
-                @Override
-                public String toString() {
-                    return "stew_cooking";
-                }
-            });
+            SERIALIZERS.register("stew_cooking", () -> StewCookingRecipe.Serializer.INSTANCE);
 
     public static void register(IEventBus eventBus) {
         SERIALIZERS.register(eventBus);
-        TYPES.register(eventBus);
     }
 }
