2660063c0f844c55ec204418429164e0db9a2828
Noah Stevens <stevens_nas@yahoo.com>
2025-12-26 20:03:52 -0500

    Add entries for tier 2 and 3 stews

--- src/main/java/io/github/skeffy/skeffystew/item/custom/ModFoodProperties.java
+++ src/main/java/io/github/skeffy/skeffystew/item/custom/ModFoodProperties.java
@@ -5,12 +5,17 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoodProperties {
-    public static final FoodProperties STEW_BASE = new FoodProperties.Builder().nutrition(2)
+    public static final FoodProperties STEW_BASE = new FoodProperties.Builder().alwaysEat().nutrition(2)
             .saturationMod(1f).build();
-    public static final FoodProperties HEALING_STEW = new FoodProperties.Builder().nutrition(4)
+    public static final FoodProperties TIER_2_STEW_BASE = new FoodProperties.Builder().alwaysEat().nutrition(3)
+            .saturationMod(1f).build();
+    public static final FoodProperties TIER_3_STEW_BASE = new FoodProperties.Builder().alwaysEat().nutrition(4)
+            .saturationMod(1f).build();
+
+    public static final FoodProperties HEALING_STEW = new FoodProperties.Builder().alwaysEat().nutrition(4)
             .saturationMod(1f).effect(() -> new MobEffectInstance(MobEffects.HEAL, 1), 100).build();
-    public static final FoodProperties BLINDING_STEW = new FoodProperties.Builder().nutrition(4)
+    public static final FoodProperties BLINDING_STEW = new FoodProperties.Builder().alwaysEat().nutrition(4)
             .saturationMod(1f).effect(() -> new MobEffectInstance(MobEffects.BLINDNESS, 24000), 100).build();
-    public static final FoodProperties HEARTY_STEW = new FoodProperties.Builder().nutrition(12)
+    public static final FoodProperties HEARTY_STEW = new FoodProperties.Builder().alwaysEat().nutrition(12)
             .saturationMod(1.5f).effect(() -> new MobEffectInstance(MobEffects.SATURATION, 1200), 100).build();
 }
