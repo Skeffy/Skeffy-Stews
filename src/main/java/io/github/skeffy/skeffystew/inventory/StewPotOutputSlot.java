e650b382b4d6067ffc2f60fdd2cdb310656d66a6
Noah Stevens <stevens_nas@yahoo.com>
2026-01-06 19:56:14 -0500

    Override onTake so taking completed recipes now pops experience

--- src/main/java/io/github/skeffy/skeffystew/inventory/StewPotOutputSlot.java
+++ src/main/java/io/github/skeffy/skeffystew/inventory/StewPotOutputSlot.java
@@ -1,19 +1,32 @@
 package io.github.skeffy.skeffystew.inventory;
 
+import io.github.skeffy.skeffystew.block.entity.StewPotBlockEntity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 import org.jetbrains.annotations.NotNull;
 
 public class StewPotOutputSlot extends SlotItemHandler {
+    private final StewPotBlockEntity entity;
 
-
-    public StewPotOutputSlot(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
+    public StewPotOutputSlot(IItemHandler itemHandler, StewPotBlockEntity entity, int index, int xPosition, int yPosition) {
         super(itemHandler, index, xPosition, yPosition);
+        this.entity = entity;
     }
 
     @Override
     public boolean mayPlace(@NotNull ItemStack pStack) {
         return false;
+    }
+
+    @Override
+    public void onTake(Player pPlayer, ItemStack pStack) {
+        this.checkTakeAchievements(pStack);
+        if(pPlayer instanceof ServerPlayer) {
+            entity.awardUsedRecipesAndPopExperience((ServerPlayer) pPlayer);
+        }
+        super.onTake(pPlayer, pStack);
     }
 }
