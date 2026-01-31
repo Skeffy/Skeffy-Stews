d04d740965fd551cb65715ee05ad3b62927f8efa
Noah Stevens <stevens_nas@yahoo.com>
2026-01-31 14:43:41 -0500

    New empty effect for experience stew

---
+++ src/main/java/io/github/skeffy/skeffystew/effect/ExperienceEffect.java
@@ -0,0 +1,10 @@
+package io.github.skeffy.skeffystew.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class ExperienceEffect extends MobEffect {
+    protected ExperienceEffect(MobEffectCategory pCategory, int pColor) {
+        super(pCategory, pColor);
+    }
+}
