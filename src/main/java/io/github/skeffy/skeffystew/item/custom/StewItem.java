d1442192183fa9cc988021b1cf43d96c1f161dd8
Noah Stevens <stevens_nas@yahoo.com>
2026-02-10 21:37:18 -0500

    Item type no longer stacks. Rewrite finishUsingItem

--- src/main/java/io/github/skeffy/skeffystew/item/custom/StewItem.java
+++ src/main/java/io/github/skeffy/skeffystew/item/custom/StewItem.java
@@ -1,17 +1,14 @@
 package io.github.skeffy.skeffystew.item.custom;
 
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BowlFoodItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 public class StewItem extends BowlFoodItem {
-    private static final int MAX_STACK_SIZE = 16;
+    private static final int MAX_STACK_SIZE = 1;
 
     public StewItem(Properties pProperties) {
         super(pProperties);
@@ -29,33 +26,7 @@ public class StewItem extends BowlFoodItem {
 
     @Override
     public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving) {
-        final ItemStack DROP_ITEM = new ItemStack(Items.BOWL);
-        int stackSize = pStack.getCount();
-        pEntityLiving.eat(pLevel, pStack);
-        if(pEntityLiving instanceof Player player && !player.getAbilities().instabuild) {
-            Inventory inv = player.getInventory();
-            if(stackSize > 1) {
-                if(inv.contains(DROP_ITEM) && inv.getItem(inv.findSlotMatchingItem(DROP_ITEM)).getCount() < 64) {
-                    int slotNumber = player.getInventory().findSlotMatchingItem(DROP_ITEM);
-                    player.getInventory().getItem(slotNumber).grow(1);
-                } else {
-                    if(player.getInventory().getFreeSlot() != -1) {
-                        player.getInventory().add(DROP_ITEM);
-                    } else {
-                        player.drop(DROP_ITEM, false);
-                    }
-                }
-            } else {
-                if(inv.contains(DROP_ITEM) && inv.getItem(inv.findSlotMatchingItem(DROP_ITEM)).getCount() < 64) {
-                    int slotNumber = player.getInventory().findSlotMatchingItem(DROP_ITEM);
-                    int itemCount = inv.getItem(slotNumber).getCount() + 1;
-                    inv.setItem(slotNumber, new ItemStack(Items.AIR));
-                    return new ItemStack(Items.BOWL, itemCount);
-                }
-                return DROP_ITEM;
-            }
-        }
-        return pStack;
+        return super.finishUsingItem(pStack, pLevel, pEntityLiving);
     }
 
     @Override
--- src/main/java/io/github/skeffy/skeffystew/item/custom/Tier2StewItem.java
+++ src/main/java/io/github/skeffy/skeffystew/item/custom/Tier2StewItem.java
@@ -2,10 +2,8 @@ package io.github.skeffy.skeffystew.item.custom;
 
 import io.github.skeffy.skeffystew.item.ModItems;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 
 public class Tier2StewItem extends StewItem{
@@ -15,32 +13,7 @@ public class Tier2StewItem extends StewItem{
 
     @Override
     public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving) {
-        final ItemStack DROP_ITEM = new ItemStack(ModItems.CERAMIC_BOWL.get());
-        int stackSize = pStack.getCount();
-        pEntityLiving.eat(pLevel, pStack);
-        if(pEntityLiving instanceof Player player && !player.getAbilities().instabuild) {
-            Inventory inv = player.getInventory();
-            if(stackSize > 1) {
-                if(inv.contains(DROP_ITEM) && inv.getItem(inv.findSlotMatchingItem(DROP_ITEM)).getCount() < 64) {
-                    int slotNumber = player.getInventory().findSlotMatchingItem(DROP_ITEM);
-                    player.getInventory().getItem(slotNumber).grow(1);
-                } else {
-                    if(player.getInventory().getFreeSlot() != -1) {
-                        player.getInventory().add(DROP_ITEM);
-                    } else {
-                        player.drop(DROP_ITEM, false);
-                    }
-                }
-            } else {
-                if(inv.contains(DROP_ITEM) && inv.getItem(inv.findSlotMatchingItem(DROP_ITEM)).getCount() < 64) {
-                    int slotNumber = player.getInventory().findSlotMatchingItem(DROP_ITEM);
-                    int itemCount = inv.getItem(slotNumber).getCount() + 1;
-                    inv.setItem(slotNumber, new ItemStack(Items.AIR));
-                    return new ItemStack(ModItems.CERAMIC_BOWL.get(), itemCount);
-                }
-                return DROP_ITEM;
-            }
-        }
-        return pStack;
+        ItemStack itemstack = super.finishUsingItem(pStack, pLevel, pEntityLiving);
+        return pEntityLiving instanceof Player && ((Player)pEntityLiving).getAbilities().instabuild ? itemstack : new ItemStack(ModItems.CERAMIC_BOWL.get());
     }
 }
